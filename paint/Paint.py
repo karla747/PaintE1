f2ed0521eac9d426c3eeaf548818a572e4eec39c
karla747 <73494867+karla747@users.noreply.github.com>
2020-10-28 19:51:55 -0600

    Add files via upload

---
+++ paint/Paint.py
@@ -0,0 +1,91 @@
+from turtle import *
+from freegames import vector
+
+def line(start, end):
+    "Draw line from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
+
+def square(start, end):
+    "Draw square from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
+
+def circle(start, end):
+    "Draw circle from start to end."
+    pass  # TODO
+
+def drawrectangle(start, end):
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+
+    fd(200)
+    rt(90)
+    fd(100)
+    rt(90)
+    fd(200)
+    rt(90)
+    fd(100)
+    rt(90)
+
+def drawtriangle(start, end):
+    "Draw triangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+
+    rt(90)
+    fd(200)
+    lt(120)
+    fd(200)
+    lt(120)
+    fd(200)
+    lt(120)
+
+def tap(x, y):
+    "Store starting point or draw shape."
+    start = state['start']
+
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+
+def store(key, value):
+    "Store value in state at key."
+    state[key] = value
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+onkey(undo, 'u')
+onkey(lambda: color('black'), 'K')
+onkey(lambda: color('white'), 'W')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('gold'), 'D')
+onkey(lambda: color('pink'), 'P')
+onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
+onkey(lambda: store('shape', line), 'l')
+onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', drawcircle), 'c')
+onkey(lambda: store('shape', drawrectangle), 'r')
+onkey(lambda: store('shape', drawtriangle), 't')
+done()
---
+++ paint/README.md
@@ -0,0 +1,2 @@
+# paint
+Archivo inicial Paint.py para actividad incial de Github en Semana Tec
